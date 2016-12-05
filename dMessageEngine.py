31243db391db76e8492b14c3203d43d83c27c04d
FursProg19 <7713002@gmail.com>
2016-12-05 18:10:09 +0200

    Начал исправлять изменения языков

--- dMessageEngine.py
+++ dMessageEngine.py
@@ -1,17 +1,26 @@
-
+alphabet_eng = [ letter for letter in range(ord('a'), ord('z') + 1)]
+alphabet_rus = [ letter for letter in range(ord('а'), ord('я') + 1)]
+alphabet_deu = ['a']+['ä']+[chr (l) for l in range(ord('b'), ord('o') + 1)]+['ö']+[chr(l) for l in range(ord('p'), ord('s') + 1)]+['ß','t','u','ü','v','w','x','y','z']
+alphabet_deu = [ord(letter) for letter in alphabet_deu]
 
 class DMessageEngine():
     #КОНСТРУКТОР КЛАССА
-    def __init__(self, key):
+    def __init__(self, key, lenguage ):
         self.key = key
-        alphabet_eng = [ letter for letter in range(ord('a'), ord('z') + 1)]
-        alphabet_rus = [ letter for letter in range(ord('а'), ord('я') + 1)]
-        self.alphabets = [alphabet_eng, alphabet_rus]
         
+        self.lenguages =   {'alphabet_eng': alphabet_eng,
+                            'alphabet_rus': alphabet_rus,
+                            'alphabet_deu': alphabet_deu}   
+        self.lenguage = lenguage
+                 
+    
+  #  print( alphabet_now (self, self.rus))
+    
     def transform (self, letter, key):
+        
         selected_alphabet = []
-        isupper = letter.isupper()
-        ord_letter = ord(letter.lower())
+        low = letter.lower()
+        ord_letter = ord(low)
                     
         for alphabet in self.alphabets:
             if ord_letter in alphabet:
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from dMessageEngine import DMessageEngine as dme
 # Вызов конструктора класса DMessageEngine с параметром 10 (key)
-engine = dme(10)
-engine1 = dme(15)
+engine = dme(10,)
+engine1 = dme(15,)
 # ciphered присвоили результат шифрования методом cipher "hello world" (message) 
 #ciphered = engine.cipher("hello world")
 # Дописать
 #deciphered = engine.decipher(ciphered)
 
-engine.report("Меня зовут Лёня!")
-engine1.report("Меня зовут Лёня!")
+engine1.report("über")
+engine.report("Ich heiße Leonid!!")
 
 engine.report("very good???")
 engine.key = 15
